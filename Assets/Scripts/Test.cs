a1243401f7e6a08bbbf60e7d7e64a02be7d84552
刘希 <liuxi@aobi.com>
2019-01-04 18:56:32 +0800

    2019.1.4

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class Test : MonoBehaviour
 {
 
     Button btn;
+    Image img;
+    Sprite sprite1;
+    Sprite sprite2;
 
 
     public int a = 10;
@@ -27,6 +31,17 @@ public class Test : MonoBehaviour
     void InitUI()
     {
         btn = GameObject.Find("UIRoot/MyBtn").GetComponent<Button>();
+        img = GameObject.Find("UIRoot/Image").GetComponent<Image>();
+
+        // sprite1 = Resources.Load<Sprite>("img1");
+        // sprite2 = Resources.Load<Sprite>("img1");
+
+        sprite1 = AssetDatabase.LoadAssetAtPath("Assets/GameAssets/Atlas/img1.png", typeof(Sprite)) as Sprite;
+        sprite2 = AssetDatabase.LoadAssetAtPath("Assets/GameAssets/Atlas/img2.png", typeof(Sprite)) as Sprite;
+
+
+
+        img.sprite = sprite1;
     }
 
     void RegisterEvent()
@@ -34,9 +49,13 @@ public class Test : MonoBehaviour
         btn.onClick.AddListener(OnClickBtn);
     }
 
+    public static int num = 0;
     void OnClickBtn()
     {
-        Debug.Log(Mathf.Clamp(this.a, 1, 3));
+        // Debug.Log(Mathf.Clamp(this.a, 1, 3));
+        num ++;
+        img.sprite = num % 2 == 0 ? null : sprite2;
+
         Debug.Log("OnClickBtn");
     }
 }
